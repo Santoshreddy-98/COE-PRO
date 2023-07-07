e654dea19a1ccb68a2fb845af2e332330c3f15fc
Vasikaran Prasad <7394@Tessolveindia.com>
2023-07-07 16:55:47 +0530

    merging

--- coe-client/src/App.js
+++ coe-client/src/App.js
@@ -1,20 +1,58 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import { MainPage } from "./DD/components/MainPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './DA/components/Login';
+import AfterLogin from './DA/components/AfterLogin';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Router>
-
-        <Routes>
-          {/* <Route exact path="/" element={} /> */}
-          <Route exact path="/landingpage" element={<MainPage />} />
-          {/* <Route exact path="/flowmanager" element={} /> */}
-        </Routes>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<AfterLogin />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import React, { useState } from 'react';
+// import AddRunDetails from './DA/components/AddRunDetails';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import LoginForm from './DA/components/LoginForm';
+
+// function App() {
+//   const [isLoggedIn, setLoggedIn] = useState(false);
+
+//   const handleLogin = () => {
+//     setLoggedIn(true);
+//   };
+  
+//   return (
+//     <div className="App">
+//      <AddRunDetails />
+//      <LoginForm />
+//      <ToastContainer position="top-right" autoClose={3000} />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ coe-client/src/DA/components/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/login', { username, password });
+      console.log(response.data); // Handle the response data as needed
+
+      // Redirect to the desired page after successful login
+      navigate('/dashboard');
+    } catch (error) {
+      setError('Invalid username or password');
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={handleUsernameChange} placeholder="Username" />
+        <input type="password" value={password} onChange={handlePasswordChange} placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
